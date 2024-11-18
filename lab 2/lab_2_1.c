a8a71e502ca47bdf9e25f6416868fe85a007a38f
GRKurushin <georgiykurushin@gmail.com>
2024-11-19 01:14:42 +0300

    add lab 1

--- lab 2/lab_2_1.c
+++ lab 2/lab_2_1.c
@@ -14,15 +14,11 @@ int main() {
     scanf("%d %d", &x3, &y3);
     printf("Enter the coordinates of the point D: ");
     scanf("%d %d", &x4, &y4);
-    // Вычислим длину сторон, диагоналей и синус угла между диагоналями
-    float AB, BC, CD, AD, d1, d2, cos, sin;
-    AB = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-    BC = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
-    CD = sqrt(pow(x4 - x3, 2) + pow(y4 - y3, 2));
-    AD = sqrt(pow(x4 - x1, 2) + pow(y4 - y1, 2));
+    // Вычислим длину диагоналей и синус угла между диагоналями
+    float d1, d2, cos, sin;
     d1 = sqrt(pow(x4 - x2, 2) + pow(y4 - y2, 2));
     d2 = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2));
-    cos = (float) ((x4-x2)*(x3-x1) + (y4-y2)*(y3-y1)) / (sqrt(pow(x4-x2,2) + pow(y4-y2,2)) * sqrt(pow(x3-x1,2) + pow(y3-y1,2)));
+    cos = ((x4-x2)*(x3-x1) + (y4-y2)*(y3-y1)) / (sqrt(pow(x4-x2,2) + pow(y4-y2,2)) * sqrt(pow(x3-x1,2) + pow(y3-y1,2)));
     sin = sqrt(1 - pow(cos, 2));
     // Вычислим площадь произвольного четырехугольника
     float S = (d1 * d2 * sin) / 2;
